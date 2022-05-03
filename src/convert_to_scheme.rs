15592e7e4ae965cfa49c82e0dc07f7e3b21191bb
YoavNir99 <cocoos9@gmail.com>
2022-05-03 18:19:16 +0300

    split into more modules

---
+++ src/convert_to_scheme.rs
@@ -0,0 +1,52 @@
+use image::*;
+use hex::*;
+use crate::dir::*;
+
+pub fn convert(file:&String,scheme: &Vec<[u8;3]>, percent:&f64) {
+    let mut image = image::open(file).unwrap().into_rgb8();
+
+        // convert image
+        for pixel in image.pixels_mut() {
+            *pixel = Rgb(pixel_to_scheme(&scheme,&[pixel[0],pixel[1],pixel[2]],*percent))
+        }
+        let file_name = return_file_name(&file);
+        image.save(format!("{}_converted.{}",file_name,return_file_ext(file))).unwrap();
+        println!("{} was converted successfully",return_short_file_name(&file));
+}
+
+pub fn pixel_to_scheme(scheme:&Vec<[u8;3]>,pixel:&[u8;3],percent:f64) -> [u8;3] {
+    let mut closest = (scheme[0].clone(),distance(&scheme[0],&pixel));
+    for color in scheme {
+        let current_distance = distance(&color,&pixel);
+        if current_distance<closest.1 {
+            closest = (color.clone(),current_distance)
+        }
+    }
+    return average(&closest.0,&pixel,percent)
+}
+
+pub fn hexes_to_scheme(hexes:Vec<String>) -> Vec<[u8;3]> {
+    let mut res = Vec::new();
+    let mut temp_arr = [0_u8;3];
+    for hex in hexes {
+        if !hex.is_empty() {
+            decode_to_slice(&hex[1..],&mut temp_arr).unwrap();
+            res.push(temp_arr.clone())
+        }
+    }
+    return res
+}
+
+pub fn distance(reference:&[u8;3],pixel:&[u8;3]) -> u8 {
+    return ((reference[0] as f64 - pixel[0] as f64).powf(2.0)+
+    (reference[1] as f64 - pixel[1] as f64).powf(2.0)+
+    (reference[2] as f64 - pixel[2] as f64).powf(2.0))
+    .sqrt() as u8
+}
+
+pub fn average(pixel1:&[u8;3],pixel2:&[u8;3],percent:f64) -> [u8;3] {
+    return [(pixel1[0] as f64 * percent+pixel2[0] as f64 * (1.0-percent)) as u8,
+            (pixel1[1] as f64 * percent+pixel2[1] as f64 * (1.0-percent)) as u8,
+            (pixel1[2] as f64 * percent+pixel2[2] as f64 * (1.0-percent)) as u8]
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use image::*;
-use hex::*;
 mod parse_text;
 use parse_text::*;
+mod convert_to_scheme;
+use convert_to_scheme::*;
 mod dir;
 use dir::*;
 use std::io::prelude::*;
@@ -29,57 +29,4 @@ fn main() {
 
     println!("{} files were converted",files.len());
     
-}
-
-
-
-
-
-
-fn convert(file:&String,scheme: &Vec<[u8;3]>, percent:&f64) {
-    let mut image = image::open(file).unwrap().into_rgb8();
-
-        // convert image
-        for pixel in image.pixels_mut() {
-            *pixel = Rgb(pixel_to_scheme(&scheme,&[pixel[0],pixel[1],pixel[2]],*percent))
-        }
-        let file_name = return_file_name(&file);
-        image.save(format!("{}_converted.{}",file_name,return_file_ext(file))).unwrap();
-        println!("{} was converted successfully",return_short_file_name(&file));
-}
-
-fn pixel_to_scheme(scheme:&Vec<[u8;3]>,pixel:&[u8;3],percent:f64) -> [u8;3] {
-    let mut closest = (scheme[0].clone(),distance(&scheme[0],&pixel));
-    for color in scheme {
-        let current_distance = distance(&color,&pixel);
-        if current_distance<closest.1 {
-            closest = (color.clone(),current_distance)
-        }
-    }
-    return average(&closest.0,&pixel,percent)
-}
-
-fn hexes_to_scheme(hexes:Vec<String>) -> Vec<[u8;3]> {
-    let mut res = Vec::new();
-    let mut temp_arr = [0_u8;3];
-    for hex in hexes {
-        if !hex.is_empty() {
-            decode_to_slice(&hex[1..],&mut temp_arr).unwrap();
-            res.push(temp_arr.clone())
-        }
-    }
-    return res
-}
-
-fn distance(reference:&[u8;3],pixel:&[u8;3]) -> u8 {
-    return ((reference[0] as f64 - pixel[0] as f64).powf(2.0)+
-    (reference[1] as f64 - pixel[1] as f64).powf(2.0)+
-    (reference[2] as f64 - pixel[2] as f64).powf(2.0))
-    .sqrt() as u8
-}
-
-fn average(pixel1:&[u8;3],pixel2:&[u8;3],percent:f64) -> [u8;3] {
-    return [(pixel1[0] as f64 * percent+pixel2[0] as f64 * (1.0-percent)) as u8,
-            (pixel1[1] as f64 * percent+pixel2[1] as f64 * (1.0-percent)) as u8,
-            (pixel1[2] as f64 * percent+pixel2[2] as f64 * (1.0-percent)) as u8]
 }
